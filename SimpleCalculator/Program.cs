5e52e1ce83b88170556b2d40c00cd5dc83d70e37
Martijn Meere <martyn.meere@gmail.com>
2017-07-11 14:56:16 -0400

    Add project files.

---
+++ SimpleCalculator/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
+
+namespace SimpleCalculator
+{
+    class Program
+    {
+        private static CompositionContainer _container;
+
+        static void Main(string[] args)
+        {
+            //An aggregate catalog that combines multiple catalogs  
+            var catalog = new AggregateCatalog();
+            //Adds all the parts found in the same assembly as the Program class  
+            catalog.Catalogs.Add(new AssemblyCatalog(typeof(Program).Assembly));
+
+            //Create the CompositionContainer with the parts in the catalog  
+            _container = new CompositionContainer(catalog);
+
+            //Fill the imports of this object  
+            try
+            {
+                _container.ComposeParts();
+            }
+            catch (CompositionException compositionException)
+            {
+                Console.WriteLine(compositionException.ToString());
+            }
+        }
+    }
+}
